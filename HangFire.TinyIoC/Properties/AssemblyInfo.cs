18ae9b0761d40f4d0d6b6a860c83f32e1dcef579
Richard Clement <richclement@gmail.com>
2014-09-03 07:39:02 -0400

    setting up nuget spec file

--- HangFire.TinyIoC/Properties/AssemblyInfo.cs
+++ HangFire.TinyIoC/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("HangFire.TinyIoC")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("TinyIoC Container support for HangFire")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("HangFire.TinyIoC")]
